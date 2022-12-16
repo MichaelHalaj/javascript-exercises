1fa21ccc979c299f73fce739d5b4e3eb28b24d16
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-16 12:02:10 -0500

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split('');
+    let l = 0, r = arr.length-1;
+    while(l < r){
+        let temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        l++;
+        r--;
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
