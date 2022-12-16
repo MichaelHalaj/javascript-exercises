92d468ca0cc2ab2698d34fc44b0cce1f59b9308f
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-16 12:26:43 -0500

    Create function that works for numbers but not strings

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for(let i = 1; i < arguments.length; i++){
+        const left = array.slice(0, arguments[i]-1);
+        const right = array.slice(arguments[i]);
+        array = left.concat(right);
+    }
+    return array;
 };
 
 // Do not edit below this line
