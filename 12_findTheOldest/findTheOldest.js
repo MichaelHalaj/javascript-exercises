e605af87c70aebc84ac3417b64d9c589877da567
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 22:18:37 -0500

    Complete find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return oldest = people.reduce((max, person) =>{
+        const maxDeath = max.yearOfDeath ? max.yearOfDeath : (new Date()).getFullYear();
+        const maxLife = maxDeath - max.yearOfBirth;
+        const deathYear = person.yearOfDeath ? person.yearOfDeath : (new Date()).getFullYear();
+        const personLife = deathYear - person.yearOfBirth;
+        if(personLife > maxLife){
+            max = person;
+        }
+        return max;
+    }, people[0]);
 };
 
 // Do not edit below this line
