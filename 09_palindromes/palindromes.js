a158a44e0e443ec7cb4a732faac78169832b8718
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 21:24:00 -0500

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let l = 0, r = string.length-1;
+    while(l < r){
+        if(string.charAt(l).toUpperCase() == string.charAt(l).toLowerCase()){
+            l++;
+            continue;
+        }
+        if(string.charAt(r).toUpperCase() == string.charAt(r).toLowerCase()){
+            r--;
+            continue;
+        }
+        if(string.charAt(l).toLowerCase() !== string.charAt(r).toLowerCase()){
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
